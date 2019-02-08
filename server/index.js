27494762013751693393996fb8b2164eb5cd6e08
ANURAGVASI <vasi.anurag@gmail.com>
2019-02-08 16:19:29 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express =require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-const PORT = process.env.PORT || 4000;
+const cors = require("cors");s
 const UserSchema = require("./models/user");
 
 // connecting to database
-mongoose.connect("mongodb://multicontainerappDB1:anurag61@docdb-2019-02-08-07-08-45.c17ern98ziqh.us-east-1.docdb.amazonaws.com:27017/test",
-  {useNewUrlParser: true, reconnectTries: 2, reconnectInterval:10 },
+mongoose.connect("mongodb://multicontainerappDB1:anurag61@docdb-2019-02-08-07-08-45.cluster-c17ern98ziqh.us-east-1.docdb.amazonaws.com:27017/?ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0",
   (err) => {
     if(err) {
         console.log("error connecting to mongodb", err);
@@ -22,8 +20,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log("server running on port 3000");
+app.listen(4000, () => {
+    console.log("server running on port 4000");
 });
 
 app.get("/", (req,res) => {
